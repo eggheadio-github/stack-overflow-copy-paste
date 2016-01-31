7b95d212f6120d8e87b72615864e84959bc0401b
Kent C. Dodds <kent@doddsfamily.us>
2016-01-30 22:49:52 -0700

    WIP: Cannot get coverage quite there

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import flatten from './flatten'
 import getQueryStringParam from './get-query-string-param'
 import snakeToCamel from './snake-to-camel'
+import padLeft from './pad-left'
+
 
 export {
   flatten,
   snakeToCamel,
   getQueryStringParam,
+  padLeft,
 }
+
