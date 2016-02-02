aa61ea0a92e617d37dad271ad16715a9aa950b82
Kent C. Dodds <kent@doddsfamily.us>
2016-02-01 20:52:06 -0700

    I need help with the commit message

--- test/pad-left.test.js
+++ test/pad-left.test.js
@@ -18,3 +18,11 @@ test('defaults to pad a zero', t => {
   t.same(actual, expected)
 })
 
+test('does not pad a string longer than the pad length', t => {
+  const original = '1234'
+  const expected = '1234'
+  const padLength = 3
+  const actual = padLeft(original, padLength)
+  t.same(actual, expected)
+})
+
