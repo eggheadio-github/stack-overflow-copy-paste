26e4d93e286a6c01341db38886d57fa08550f381
shenseye <shenseye@gmail.com>
2016-02-19 17:24:06 +0700

    test: Add a test for deep flatten

--- test/flatten.test.js
+++ test/flatten.test.js
@@ -7,3 +7,10 @@ test('flattens an array of arrays', t => {
   const actual = flatten(original)
   t.same(actual, expected)
 })
+
+test('deep flattens an array of arrays', t => {
+  const original = [1, 2, [3, 4, [5, 6], 7], 8]
+  const expected = [1, 2, 3, 4, 5, 6, 7, 8]
+  const actual = flatten(original)
+  t.same(actual, expected)
+})
