b818e29264da0c9780fa9119295adf4807f2244d
anup <anupmohan2@gmail.com>
2016-09-18 22:24:08 -0500

    WIP:Fixed lint error but now the regex fails

--- src/validateEmail.js
+++ src/validateEmail.js
@@ -1,7 +1,10 @@
 export default validateEmail
 
-const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-
+const regex = new RegExp(['^(([^<>()[]\\.,;:\\s@"]+(\\.[^<>(),[]\\.,;:\\s@"]+)*)',
+                        '|(\\".+\\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.',
+                        '[0-9]{1,3}])|(([a-zA-Z-0-9]+\\.)+',
+                        '[a-zA-Z]{2,}))$'].join(''))
+             
 /**
  * Orginal Source: http://stackoverflow.com/questions/46155
  * This method will return an array with the given value prefilled
@@ -9,6 +12,6 @@ const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\
  * @param {String} email - The email address to be validated
  * @return {Boolean} - True if the string passes the regex else False
  */
-function validateEmail(email){
+function validateEmail(email) {
   return regex.test(email)
 }
