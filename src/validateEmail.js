9c9c8cc1cefeaf5f0e8884162f1f96b44d3331d9
anup <anupmohan2@gmail.com>
2016-09-19 14:23:39 -0500

    feat(validateEmail): Add validateEmail function

--- src/validateEmail.js
+++ src/validateEmail.js
@@ -1,10 +1,7 @@
 export default validateEmail
 
-const regex = new RegExp(['^(([^<>()[]\\.,;:\\s@"]+(\\.[^<>(),[]\\.,;:\\s@"]+)*)',
-                        '|(\\".+\\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.',
-                        '[0-9]{1,3}])|(([a-zA-Z-0-9]+\\.)+',
-                        '[a-zA-Z]{2,}))$'].join(''))
-             
+const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ // eslint-disable-line max-len
+
 /**
  * Orginal Source: http://stackoverflow.com/questions/46155
  * This method will return an array with the given value prefilled
