ee7afb3bd3f2680407f65e51183b3dc6a4432b44
Sagar Bharadwaj <sagar.bharadwaj@yahoo.com>
2016-10-29 21:04:40 +0530

    Changed validateEmail.js

--- src/validateEmail.js
+++ src/validateEmail.js
@@ -1,7 +1,5 @@
 export default validateEmail
 
-const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ // eslint-disable-line max-len
-
 /**
  * Orginal Source: http://stackoverflow.com/questions/46155
  * This method will return an array with the given value prefilled
@@ -10,5 +8,6 @@ const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\
  * @return {Boolean} - True if the string passes the regex else False
  */
 function validateEmail(email) {
-  return regex.test(email)
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
 }
