df5635dcef090a7672d5b2405debeaba3a70dc12
= <shaikanjavali.mastan@a-cti.com>
2016-12-19 12:49:16 +0530

    fix: Handled code coverage

--- test/add.test.js
+++ test/add.test.js
@@ -1,10 +1,10 @@
 import test from 'ava'
-import {add} from '../src'
+import {additon} from '../src'
 
 test('Adds two integers ', t => {
   const number1 = 23
   const number2 = 3
-  const actual = 26
-  const expected = add(number1, number2)
+  const expected = 23 + 3
+  const actual = additon(number1, number2)
   t.deepEqual(actual, expected)
 })
