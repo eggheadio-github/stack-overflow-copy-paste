b4327c2528bc66f587cc591a5fdf41be3e218d99
Arup Sen <arup@arupdesigns.co.uk>
2017-01-29 01:14:50 +0000

    feat(dec2bin): Add decimal to binary function
    Closes #69

--- src/dec2bin.js
+++ src/dec2bin.js
@@ -7,12 +7,11 @@ export default dec2bin
  * Floating point numbers truncated to integer
  *
  * @param {String} dec - string version of integer
- * @param {Number} dec - number version of integer
  * @return {String} - binary version of input as string
  */
 function dec2bin(dec) {
   if (parseInt(dec, 10) < 0) {
-    return "Only works for positive integers"
+    return 'Only works for positive integers'
   } else {
     return parseInt(dec, 10).toString(2)
   }
