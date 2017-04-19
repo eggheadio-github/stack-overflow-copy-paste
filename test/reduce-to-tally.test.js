a1311a605564f08ce398c686741e8fb574526ce7
Priyanka <priyanka3007@gmail.com>
2017-04-18 20:39:28 -0700

    fix(review): addressed review comments
    replaced double quotes with single

--- test/reduce-to-tally.test.js
+++ test/reduce-to-tally.test.js
@@ -3,8 +3,8 @@ import reducer from '../src/reduce-to-tally'
 
 test('tallies an array into object with count on each', t => {
   const initialvalue = {}
-  const original = ["angular", "angular", "rails", "angular", "Python", "angular", "Python", "angular", "Vanilla"]
-  const expected = {"Python": 2, "Vanilla": 1, "angular": 5, "rails": 1}
+  const original = ['angular', 'angular', 'rails', 'angular', 'Python', 'angular', 'Python', 'angular', 'Vanilla']
+  const expected = {Python: 2, Vanilla: 1, angular: 5, rails: 1}
   const actual = original.reduce(reducer, initialvalue)
   t.deepEqual(actual, expected)
 })
