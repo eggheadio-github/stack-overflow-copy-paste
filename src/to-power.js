af61ca487201137bdcf92db3c27a2d8faeee3281
Brett East <brett@sproutback.com>
2017-04-23 18:44:14 +1000

    feat(toPower): Add toPower function
    Closes #80

---
+++ src/to-power.js
@@ -0,0 +1,12 @@
+export default toPower
+
+/**
+ * This method will perform a 'to power' operation.
+ *
+ * @param {Number} num1 - the base number for the calculation
+ * @param {Number} num2 - the exponent for the calculation
+ * @return {Number} - Result of toPower
+ */
+function toPower(num1, num2) {
+  return num1 ** num2
+}
---
+++ test/to-power.test.js
@@ -0,0 +1,34 @@
+import test from 'ava'
+import {toPower} from '../src'
+
+test('calculate base number to exponent ', t => {
+  const number1 = 3
+  const number2 = 4
+  const expected = 3**4
+  const actual = toPower(number1, number2)
+  t.deepEqual(actual, expected)
+})
+
+test('works for negative exponents ', t => {
+  const number1 = 8
+  const number2 = -2
+  const expected = 8**-2
+  const actual = toPower(number1, number2)
+  t.deepEqual(actual, expected)
+})
+
+test('works for base 0 ', t => {
+  const number1 = 0
+  const number2 = 4
+  const expected = 0**4
+  const actual = toPower(number1, number2)
+  t.deepEqual(actual, expected)
+})
+
+test('works for exponent 0 ', t => {
+  const number1 = 8
+  const number2 = 0
+  const expected = 8**0
+  const actual = toPower(number1, number2)
+  t.deepEqual(actual, expected)
+})
