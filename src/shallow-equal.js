fd5b7d2e33d41fe56a0c83d8ec1cfc6cd6c177e9
Łukasz Leliński <lukasz@lelinski.pl>
2017-05-11 21:50:23 +0200

    Reduce cyclomatic complexity

--- src/shallow-equal.js
+++ src/shallow-equal.js
@@ -1,29 +1,17 @@
 export default shallowEqual
 
-/**
- * This method will shallow compare two objects
- *
- * @param {Object} objA - first object to compare
- * @param {Object} objB - second object to compare
- * @returns {boolean} - result of comparison - true if objects are shallow equal
- */
-function shallowEqual(objA, objB) {
-  if (objA === objB) {
-    return true
-  }
+function haveEqualKeys(a, b) {
+  const keysA = Object.keys(a)
+  const keysB = Object.keys(b)
+  return keysA.length === keysB.length
+}
 
-  if (typeof objA !== 'object' || objA === null ||
-    typeof objB !== 'object' || objB === null) {
-    return false
-  }
+function isNullOrNonObject(obj) {
+  return typeof obj !== 'object' || obj === null
+}
 
+function compareKeys(objA, objB) {
   const keysA = Object.keys(objA)
-  const keysB = Object.keys(objB)
-
-  if (keysA.length !== keysB.length) {
-    return false
-  }
-
   const bHasOwnProperty = Object.prototype.hasOwnProperty.bind(objB)
   for (let i = 0; i < keysA.length; i++) {
     if (!bHasOwnProperty(keysA[i]) || objA[keysA[i]] !== objB[keysA[i]]) {
@@ -32,4 +20,24 @@ function shallowEqual(objA, objB) {
   }
 
   return true
+}
+
+/**
+ * This method will shallow compare two objects
+ *
+ * @param {Object} objA - first object to compare
+ * @param {Object} objB - second object to compare
+ * @returns {boolean} - result of comparison - true if objects are shallow equal
+ */
+
+function shallowEqual(objA, objB) {
+  if (objA === objB) {
+    return true
+  }
+
+  if (isNullOrNonObject(objA) || isNullOrNonObject(objB) || !haveEqualKeys(objA, objB)) {
+    return false
+  }
+
+  return compareKeys(objA, objB)
 }
