0c8605dd12fe2e01730c421ccdc633133eb0b9d5
Zebulon McCorkle <zebmccorkle@gmail.com>
2017-05-27 08:12:45 -0500

    fix(startsWith): correct spelling of Boolean (#92)

--- src/startsWith.js
+++ src/startsWith.js
@@ -7,7 +7,7 @@ export default startsWith
  *
  * @param {String} str - The string to validate against
  * @param {String} head - The input to match with str substring
- * @return {Bollean} - True if 'str' starts with 'head', otherwise false
+ * @return {Boolean} - True if 'str' starts with 'head', otherwise false
  */
 function startsWith(str, head) {
   const tmp = str.substring(0, head.length)
