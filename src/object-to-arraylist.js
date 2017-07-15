b59c4ecacce7d823c21485a99ae89607564120df
Edward Dunn <edwarddunn11@hotmail.co.uk>
2017-07-15 07:20:18 +0100

    fix(object-to-arraylist) : correct indent and naming

--- src/object-to-arraylist.js
+++ src/object-to-arraylist.js
@@ -1,15 +1,15 @@
-export default object-to_arraylist
+export default convertObj
 
 /**
  * This method will place the contents of an object into an arraylist
  *
  * @param {Object} obj - object to convert
  */
-    function convertObj(obj) {
-        var result = [];
-        for (var key in obj) {
-            result.push([key, obj[key]])
-        }
-        return result;
-    }
+function convertObj(obj) {
+	var result = [];
+	for (var key in obj) {
+		result.push([key, obj[key]])
+	}
+	return result;
+}
     
