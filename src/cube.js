91bbd718ea3576c89ba9ce5b2ed426fe850f8baa
Aswin Koliyot <akoliyot@gmail.com>
2017-10-03 12:34:44 +0530

    feat(cube): Add cube function
    Add a function to find the cube of a number.

---
+++ src/cube.js
@@ -0,0 +1,12 @@
+export default cube
+
+/**
+ * This method will perform cube of a number.
+ *
+ * @param {Number} num - Input number
+ * @return {Number} - Result of cube of a number
+ */
+
+function cube(num) {
+  return num * num * num
+}
