f269f88bdfded818c08b011b1ab1a356aa8b20ed
Andrej Premrn <andrej.premrn@gmail.com>
2017-12-16 20:29:05 +0100

    simple contribution (#135)
    * WIP: First contribution.
    
    * WIP: Initial commit.

--- src/flatten.js
+++ src/flatten.js
@@ -3,7 +3,7 @@ export default flatten
 /**
  * Original Source: http://stackoverflow.com/a/15030117/971592
  *
- * This method will flatten arrays given to it.
+ * This method will flatten arrays given to it. It's a copy.
  *
  * @param {...Array} arrays - The array(s) to flatten
  * @return {Array} - The flattened array
