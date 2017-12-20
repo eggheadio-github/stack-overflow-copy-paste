6a03da3c024db090d35e24f80cca3d0c7ade67be
Hu Chen <chen@huchen.me>
2017-12-20 15:27:26 +0800

    refactor: Clean up file and avoid repeating. Sort exports

--- src/index.js
+++ src/index.js
@@ -1,103 +1,50 @@
-import isEven from './is-even'
-import revstring from './revstring'
-import initArray from './init-array'
-import reduce from './reduce-to-tally'
-import flatten from './flatten'
-import getQueryStringParam from './get-query-string-param'
-import snakeToCamel from './snake-to-camel'
-import padLeft from './pad-left'
-import randomInteger from './random-integer'
-import arrayFill from './array-fill'
-import sortObjectsArray from './sort-objects-array'
-import objectValuesToString from './object-values-to-string'
-import getObjectSize from './get-object-size'
-import isArray from './is-array'
-import validateEmail from './validateEmail'
-import hex2rgb from './hex2rgb'
-import isNullOrWhitespace from './is-null-or-whitespace'
-import isToday from './is-today'
-import startsWith from './startsWith'
-import removeDuplicates from './remove-duplicates'
-import add from './add'
-import subtract from './subtract'
-import subtraction from './subtraction'
-import divide from './divide'
-import multiply from './multiply'
-import square from './square'
-import cube from './cube'
-import sum from './sum'
-import dec2bin from './dec2bin'
-import searchAndReplace from './search-and-replace'
-import sqrt from './sqrt'
-import toPower from './to-power'
-import mod from './mod'
-import shallowEqual from './shallow-equal'
-import swapCase from './swap-case'
-import endsWith from './endsWith'
-import round from './round'
-import checkPalindrome from './checkPalindrome'
-import isFunction from './is-function'
-import isOdd from './is-odd'
-import isNumeric from './is-numeric'
-import max from './max'
-import slugify from './slugify'
-import convertToRoman from './convertToRoman'
-import gcd from './gcd'
-import range from './range'
-import contains from './contains'
-import getOrdinalSuffix from './get-ordinal-suffix'
-import arrayAverage from './array-average'
-import find from './find'
-
-export {
-  isOdd,
-  isEven,
-  revstring,
-  initArray,
-  reduce,
-  find,
-  flatten,
-  snakeToCamel,
-  getQueryStringParam,
-  padLeft,
-  randomInteger,
-  arrayFill,
-  sortObjectsArray,
-  objectValuesToString,
-  getObjectSize,
-  isArray,
-  validateEmail,
-  hex2rgb,
-  isNullOrWhitespace,
-  isToday,
-  startsWith,
-  removeDuplicates,
-  add,
-  subtract,
-  divide,
-  multiply,
-  square,
-  cube,
-  sum,
-  dec2bin,
-  searchAndReplace,
-  sqrt,
-  toPower,
-  mod,
-  shallowEqual,
-  swapCase,
-  endsWith,
-  round,
-  checkPalindrome,
-  isFunction,
-  subtraction,
-  isNumeric,
-  max,
-  slugify,
-  convertToRoman,
-  gcd,
-  range,
-  contains,
-  getOrdinalSuffix,
-  arrayAverage,
-}
+export {default as add} from './add'
+export {default as arrayAverage} from './array-average'
+export {default as arrayFill} from './array-fill'
+export {default as checkPalindrome} from './checkPalindrome'
+export {default as contains} from './contains'
+export {default as convertToRoman} from './convertToRoman'
+export {default as cube} from './cube'
+export {default as dec2bin} from './dec2bin'
+export {default as divide} from './divide'
+export {default as endsWith} from './endsWith'
+export {default as find} from './find'
+export {default as flatten} from './flatten'
+export {default as gcd} from './gcd'
+export {default as getObjectSize} from './get-object-size'
+export {default as getOrdinalSuffix} from './get-ordinal-suffix'
+export {default as getQueryStringParam} from './get-query-string-param'
+export {default as hex2rgb} from './hex2rgb'
+export {default as initArray} from './init-array'
+export {default as isArray} from './is-array'
+export {default as isEven} from './is-even'
+export {default as isFunction} from './is-function'
+export {default as isNullOrWhitespace} from './is-null-or-whitespace'
+export {default as isNumeric} from './is-numeric'
+export {default as isOdd} from './is-odd'
+export {default as isToday} from './is-today'
+export {default as max} from './max'
+export {default as mod} from './mod'
+export {default as multiply} from './multiply'
+export {default as objectValuesToString} from './object-values-to-string'
+export {default as padLeft} from './pad-left'
+export {default as randomInteger} from './random-integer'
+export {default as range} from './range'
+export {default as reduce} from './reduce-to-tally'
+export {default as removeDuplicates} from './remove-duplicates'
+export {default as revstring} from './revstring'
+export {default as round} from './round'
+export {default as searchAndReplace} from './search-and-replace'
+export {default as shallowEqual} from './shallow-equal'
+export {default as slugify} from './slugify'
+export {default as snakeToCamel} from './snake-to-camel'
+export {default as sortObjectsArray} from './sort-objects-array'
+export {default as sqrt} from './sqrt'
+export {default as square} from './square'
+export {default as startsWith} from './startsWith'
+export {default as subtract} from './subtract'
+export {default as subtraction} from './subtraction'
+export {default as sum} from './sum'
+export {default as swapCase} from './swap-case'
+export {default as toPower} from './to-power'
+export {default as validateEmail} from './validateEmail'
