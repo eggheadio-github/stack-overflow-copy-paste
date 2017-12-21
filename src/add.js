472e816e22ab4bc0cc6398a1c36bc6db9e99e732
Alex LeBron <lebron.alex@yahoo.com>
2017-12-21 14:00:23 -0500

    addeda test change

--- src/add.js
+++ src/add.js
@@ -2,11 +2,11 @@ export default add
 
 /**
  * This method will perform addition operation.
- *
+ * added this comment for a test
  * @param {Number} num1 - first number for addition
  * @param {Number} num2 - second  number for additon
  * @return {Number} - Result of addition
  */
 function add(num1, num2) {
-  return num1 + num2
+  return num1 + num2 + 0
 }
