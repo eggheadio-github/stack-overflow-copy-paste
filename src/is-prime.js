ab758b4c5f1ed5a6e8aad27631cd32d5923d6d12
Alexandru Tomescu <alex.tmsc@gmail.com>
2018-01-17 16:52:49 +0200

    WIP: Add isPrime function

---
+++ src/is-prime.js
@@ -0,0 +1,13 @@
+export default isPrime
+
+/**
+ * This method tests if a given number is prime
+ *
+ * @param {Number} number - number to be evaluated
+ * @returns {Boolean} - True if number is prime, false otherwise
+ */
+function isPrime(number) {
+  for(var i = 2; i < number; i++)
+    if(number % i === 0) return false;
+  return number >= 1;
+}
