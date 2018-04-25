78275c3c48899ef874dc5a8efd9494f45467d6fa
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2018-04-25 23:54:27 +0100

    WIP: Cannot get coverage quite there

---
+++ src/get-query-string-value.js
@@ -0,0 +1,31 @@
+export default getQueryStringValue
+
+/**
+ * Original Source: https://stackoverflow.com/questions/901115/
+ *
+ * This method will return the query string value
+ * of the given parameter name
+ *
+ * @param {String} name - The parameter name in query string
+ * @param {String} url - The url with query string
+ * @return {String} - The query string value
+ */
+
+function getQueryStringValue(name, url) {
+  if (!url) {
+    url = window.location.href
+  }
+
+  name = name.replace(/[\[\]]/g, '\\$&')
+  const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`)
+  const results = regex.exec(url)
+  if (!results) {
+    return null
+  }
+
+  if (!results[2]) {
+    return ''
+  }
+
+  return decodeURIComponent(results[2].replace(/\+/g, ' '))
+}
