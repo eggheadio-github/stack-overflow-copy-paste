87e095bdfcc6ada7f11f836cec52e6149d63adae
Shubham Tiwari <websbiz0000@gmail.com>
2018-05-05 06:04:20 +0000

    feat(common): add lessThan function with tests

---
+++ src/less-than.js
@@ -0,0 +1,12 @@
+export default lessThan
+
+/**
+ * This method will check if num1 is less than num2
+ *
+ * @param {Number} num1 - first number
+ * @param {Number} num2 - second  number
+ * @return {Boolean} - Returns true if num1 is less than num2, else false.
+ */
+function lessThan(num1, num2) {
+  return num1 < num2
+}
