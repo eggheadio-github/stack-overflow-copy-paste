3559862f59c6818e510b208a56dba9756defc7c0
Jehun Seem <sjhgenius@gmail.com>
2018-05-15 05:40:27 +0900

    chore: fix wrong word and white space (#167)

--- src/startsWith.js
+++ src/startsWith.js
@@ -3,7 +3,7 @@ export default startsWith
 /**
  * Original Source: http://stackoverflow.com/a/646631/5954939
  *
- * This method will return a bollean indicating whether a string starts with a given input.
+ * This method will return a boolean indicating whether a string starts with a given input.
  *
  * @param {String} str - The string to validate against
  * @param {String} head - The input to match with str substring
