e64ea92c7fe1f7b25a0fcfe0165a34505fcaf6fb
akash <akashagarwal0403@gmail.com>
2018-06-27 03:34:07 +0530

    adding timeConversion function

---
+++ src/timeConversion.js
@@ -0,0 +1,36 @@
+export default timeConversion;
+/**
+ *
+ * This method will convert time in 12 hours format to 24 hours format
+ *
+ * @param {String} time - time in 12 hours format example 01:12:45AM or 06:48:56PM
+ * @return {String} - converted time in 24 hours example 01:12:45 or 18:48:56
+ */
+
+function timeConversion(time){
+    var convertedTime= time.substr(2,6);
+    if(time.charAt(8)=="A")
+    {
+        if(time.charAt(0)=="1" && time.charAt(1)=="2")
+        {
+            convertedTime= "00"+convertedTime;
+        }
+        else
+        convertedTime= time.substr(0,2)+convertedTime;
+    }
+    else
+    {
+        if(time.charAt(0)=="1" && time.charAt(1)=="2")
+        {
+            convertedTime= "12"+convertedTime;
+        }
+        else{
+            var a = time.substr(0,2);
+            var b = Number(a);
+            var c = (b+12)+"";
+            convertedTime= c+convertedTime;
+
+        }
+    } 
+   return convertedTime
+}
