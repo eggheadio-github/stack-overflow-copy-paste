b67348578895b33eaf4cc6d941d94d3e7fbbec47
Kent C. Dodds <kent@doddsfamily.us>
2018-12-06 20:04:43 -0700

    stuff

--- test/last.test.js
+++ test/last.test.js
@@ -4,6 +4,5 @@ import {last} from '../src'
 test('gets the last item from an array', t => {
   const array = [1, 2, 3]
   const lastItem = last(array)
-  const expected = 3
-  t.deepEqual(lastItem, expected)
+  t.deepEqual(lastItem, 3)
 })
