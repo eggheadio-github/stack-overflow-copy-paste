c489ff99d80709d87ede0d7fbcb04510abea6fe7
Akash Rajpurohit <akash009.dev@gmail.com>
2019-02-02 12:16:16 +0530

    feat(largest): finds largest number in an array

---
+++ test/largest.test.js
@@ -0,0 +1,14 @@
+import test from 'ava'
+import { largest } from '../src'
+
+test('gets largest number from array', t => {
+  const array = [23, 45, 25, 67, 24, 86, 12, 94, 37]
+  const largestItem = largest(array)
+  t.deepEqual(largestItem, 94)
+})
+
+test('return null in case of empty array', t => {
+  const array = []
+  const largestItem = largest(array)
+  t.deepEqual(largestItem, null)
+})
